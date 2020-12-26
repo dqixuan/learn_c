8b36960124994ae26947ba98f4cbc9508a109fa9
dqx <654182528@qq.com>
2020-12-26 23:16:37 +0800

    chore

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
-int main() {
+int main1() {
     printf("hello world~\n");
+    return 0;
+}
+
+
+int main() {
+    int ten = 10;
+    int two = 2;
+
+    printf("Doing it right: ");
+    printf("%d minus %d is %d\n", ten, two, ten - two);
+    printf("Doing it wrong：");
+    printf("%d minus %d is %d\n", ten);
     return 0;
 }
